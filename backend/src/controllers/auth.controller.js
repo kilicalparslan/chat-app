4ecc4c304c08f1a908b1a1de5bc21a8884942660
Alp <kilicalparslan1998@gmail.com>
2025-05-21 15:04:02 +0300

    signup logic created w jwt

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,47 @@
-export const signup = (req, res) => {
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../utils/utils.js";
+
+export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
-    res.send("Signup Page");
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
+    }
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePicture: newUser.profilePicture,
+      });
+    } else {
+      return res.status(400).json({ message: "User creation failed" });
+    }
   } catch (error) {
     console.error("Error in signup:", error);
     res.status(500).send("Internal Server Error");
-    
   }
 };
 
@@ -16,5 +52,3 @@ export const login = (req, res) => {
 export const logout = (req, res) => {
   res.send("Logout Page");
 };
-
-
