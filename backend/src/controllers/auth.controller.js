f55b6b08b7191b7f1683ceda733bd06d9b155bef
Alp <kilicalparslan1998@gmail.com>
2025-05-22 12:57:26 +0300

    profile picture upload route & controller created

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { generateToken } from "../utils/utils.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -76,7 +77,7 @@ export const logout = (req, res) => {
   try {
     res.cookie("token", "", {
       maxAge: 0,
-    })
+    });
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
     console.error("Error in logout:", error);
@@ -85,5 +86,30 @@ export const logout = (req, res) => {
 };
 
 export const updateProfile = async (req, res) => {
-  
+  try {
+    const { profilePicture } = req.body;
+    const userId = req.user._id;
+    if (!profilePicture) {
+      return res.status(400).json({ message: "Profile picture is required" });
+    }
+    const uploadResponse = await cloudinary.v2.uploader.upload(profilePicture);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePicture: uploadResponse.secure_url },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.error("Error in updateProfile:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.error("Error in checkAuth:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,25 +1,26 @@
-import jwt from 'jsonwebtoken';
-import { User } from '../models/user.model.js';
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
 
 export const protectRoute = async (req, res, next) => {
-    try {
-        const token = req.cookies.token;
+  try {
+    const token = req.cookies.token;
 
-        if(!token) {
-            return res.status(401).json({ message: "Unauthorized, no token" });
-        }
-
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        if(!decoded) {
-            return res.status(401).json({ message: "Unauthorized, invalid token" });
-        }
-        const user = await User.findById(decoded.id).select('-password');
-        if(!user) {
-            return res.status(401).json({ message: "Unauthorized, user not found" });
-        }
-        req.user = user;
-        next();
-    } catch (error) {
-        
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized, no token" });
     }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) {
+      return res.status(401).json({ message: "Unauthorized, invalid token" });
+    }
+    const user = await User.findById(decoded.id).select("-password");
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized, user not found" });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+    console.error("Error in protectRoute:", error);
+  }
 };
